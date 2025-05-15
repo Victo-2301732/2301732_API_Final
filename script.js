75b32717ca6ec6bf2619747c1f9238267b892059
Derek <derekbernier31@outlook.com>
2025-05-15 15:02:28 -0400

    modification route docs

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import fs from 'fs';
-import path from 'path';
 
 import routeTaches from './src/routes/tache.route.js';
 import routeUtilisateurs from './src/routes/utilisateur.route.js';
@@ -15,7 +13,6 @@ app.use(morgan('dev'));
 
 app.use('/api/taches', routeTaches);
 app.use('/api/utilisateurs', routeUtilisateurs);
-app.use('/api/docs', express.static('docs'));
 
 app.use((req, res) => res.status(404).json({ erreur: 'Route non trouvée' }));
 
